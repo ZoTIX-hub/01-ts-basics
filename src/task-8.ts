dbe3e4d4370eb65d5b5b5adb87a23593b6cec437
Artem illushchenko <artemillushchenko30@gmail.com>
2026-01-08 12:05:52 +0200

    FIX

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 interface Post {
-    id: number,
-    title: string,
-    body: string,
+  id: number;
+  title: string;
+  body: string;
 }
 
-async function fetchPosts() {
+async function fetchPosts(): Promise<Post[]> {
   const response = await axios.get<Post[]>(
-    'https://jsonplaceholder.typicode.com/posts'
+    "https://jsonplaceholder.typicode.com/posts"
   );
   return response.data;
 }
